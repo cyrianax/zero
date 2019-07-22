467daba66ff1b66e06fe1614f9c92c032c7050e6
excella <excella@yeah.net>
2019-07-22 23:22:14 +0800

    gather api

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const Router = require('koa-router')
 const api = new Router()
 
 const content = require('./api.content')
+const gather = require('./api.gather')
 
 api.use('/content', content.routes(), content.allowedMethods())
+api.use('/gather', gather.routes(), gather.allowedMethods())
 
 module.exports = api
