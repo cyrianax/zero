fe964be915eb669ed611a2fc339c1f88da1a8dd3
excella <excella@yeah.net>
2019-08-01 22:30:57 +0800

    template style

--- view/controllers/content.js
+++ view/controllers/content.js
@@ -8,9 +8,12 @@ router.get('/content/:id', async ctx => {
   if (ctx.params.id) {
     const content = await Content.findById(ctx.params.id)
     if (content) {
+      console.log(content);
+      
       ctx.render('content', {
         content: {
           title: content.title,
+          createDate: moment(content.create_date).format('YYYY-MM-DD hh:mm'),
           content: marked(content.content)
         }
       })
